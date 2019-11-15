5f65efa178e221c7926f6b8507a1ed3aa17288d6
nullmicgo <nullmicog@gmail.com>
2019-11-15 23:00:04 +0800

    Deleteing item

--- App.js
+++ App.js
@@ -14,6 +14,11 @@ export default function App() {
       ]);
     };
     
+  const removeGoalHandler = goalId =>{
+    setCourseGoals(currentGoals=>{
+      return currentGoals.filter((goal)=>goal.id !== goalId);
+    });
+  }
 
 
   return (
@@ -22,7 +27,7 @@ export default function App() {
         <FlatList 
           keyExtractor={(item, index)=> item.id}
           data={courseGoals} 
-          renderItem={itemData=> ( <GoalItem onDelete={() => console.log('Does that work?')} title={itemData.item.value} /> )}
+          renderItem={itemData=> ( <GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value} /> )}
         />
     </View>
   );
--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const GoalItem = props =>{
-    return  <TouchableOpacity onPress={props.onDelete} >
+    return  <TouchableOpacity onPress={props.onDelete.bind(this, props.id)} >
         <View style={styles.listItem} >
         <Text >{props.title}</Text>
         </View>
