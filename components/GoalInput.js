7f84d65e01e8b41a55eae6efbedcb4d9d38df2cf
nullmicgo <nullmicog@gmail.com>
2019-11-15 23:19:08 +0800

    Adding style for the modal

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -11,25 +11,25 @@ const GoalInput = props =>{
 
 
     return   <Modal visible={props.visible} animationType="slide">
-        <View style={styles.inputContainer}>
-    <TextInput placeholder="Course Goal"   
-    style={styles.input}
-    onChangeText={goalInputHandler}
-    value={enteredGoal}
-     />
-    <Button 
-    title="ADD" 
-    onPress={props.onAddGoal.bind(this, enteredGoal)}
-     />
-  </View>
+                    <View style={styles.inputContainer}>
+                            <TextInput placeholder="Course Goal"   
+                            style={styles.input}
+                            onChangeText={goalInputHandler}
+                            value={enteredGoal}
+                            />
+                            <Button 
+                            title="ADD" 
+                            onPress={props.onAddGoal.bind(this, enteredGoal)}
+                            />
+            </View>
   </Modal>
 }
 
 const styles = StyleSheet.create({
     inputContainer:{
-      flexDirection: 'row', 
-      justifyContent: 'space-between', 
-      alignContent:'center'
+      flex: 1,
+      justifyContent: 'center', 
+      alignItems:'center'
     },
     input:{
       width: '80%', 
